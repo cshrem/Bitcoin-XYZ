d590cf69fe62958ed42db5f5a46d70e928298677
Andrew DeSantis <atd@bitcoin.sh>
2017-07-17 05:46:36 -0500

    update: app/bitcoin.team/main.py && templates/bitcoin.team/index.html

--- app/bitcoin.team/main.py
+++ app/bitcoin.team/main.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import web
 from htmlmin.minify import html_minify
 
+PATH   = '../../templates/bitcoin.team/'
+urls   = ('/', 'home')
+app    = web.application(urls, globals())
+render = web.template.render(PATH)
+render._add_global(render, 'render')
 
-urls = ('/(.*)', 'hello')
-app = web.application(urls, globals())
-
-
-class hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
-
+class home:
+    def GET(self):
+        return html_minify(str(render.index(data=None)))
 
 if __name__ == "__main__":
     app.run()
