f832f3c71d7c8692a998d1c9f9142397bf1db70a
Andrew DeSantis <atd@bitcoin.sh>
2017-07-17 05:39:05 -0500

    update: bitcoin.team

--- app/bitcoin.team/main.py
+++ app/bitcoin.team/main.py
@@ -1,15 +1,21 @@
-import web
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-urls = (
-    '/(.*)', 'hello'
-)
+
+import web
+from htmlmin.minify import html_minify
+
+
+urls = ('/(.*)', 'hello')
 app = web.application(urls, globals())
+
 
 class hello:
     def GET(self, name):
         if not name:
             name = 'World'
         return 'Hello, ' + name + '!'
+
 
 if __name__ == "__main__":
     app.run()
