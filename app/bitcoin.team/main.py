a4b045031e0d91083459d15e4c92128614fbbb59
Andrew DeSantis <atd@bitcoin.sh>
2017-07-17 05:29:17 -0500

    update: Makefile,var/html/bitcoin-core.html,app/bitcoin.team/main.py && rm: app/bitcoin.team/README.md && add: .gitmodules

--- app/bitcoin.team/main.py
+++ app/bitcoin.team/main.py
@@ -1,1 +1,15 @@
-print 'yo'
+import web
+
+urls = (
+    '/(.*)', 'hello'
+)
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self, name):
+        if not name:
+            name = 'World'
+        return 'Hello, ' + name + '!'
+
+if __name__ == "__main__":
+    app.run()
